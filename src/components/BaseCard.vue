44d1d32b335a246a6adad6447c71fa72b320df99
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 00:05:01 +0330

    Add initial structure of the card

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div>
+      <img src="" alt="" />
+      <h2>Jessica Randall</h2>
+      <p>London, United Kingdom</p>
+
+      <p>"Front-end developer and reader."</p>
+    </div>
+
+    <div>
+      <button>GitHub</button>
+      <button>Frontend Mentor</button>
+      <button>LinkedIn</button>
+      <button>Twitter</button>
+      <button>Instagram</button>
+    </div>
+  </section>
+</template>
