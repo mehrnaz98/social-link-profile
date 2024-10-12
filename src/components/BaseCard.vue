a422d69d9e6497cf25508477f110686fba10bdc4
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 00:42:22 +0330

    Update BaseCard.vue

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -2,7 +2,7 @@
   <section
     class="font-inter border w-[350px] h-[510px] flex flex-col justify-center items-center text-center"
   >
-    <div class="flex flex-col">
+    <div class="flex flex-col justify-center items-center">
       <img
         src="../assets/images/avatar-jessica.jpeg"
         alt="Profile picture"
