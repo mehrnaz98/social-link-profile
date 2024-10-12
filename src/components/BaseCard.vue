194b4e8c03ee3049e1ba2bde4e9c352e3e9139e1
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 00:39:55 +0330

    Update BaseCard.vue

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,7 +3,7 @@
     class="font-inter border w-[350px] h-[510px] flex flex-col justify-center items-center text-center"
   >
     <div class="flex flex-col">
-      <img src="../assets/images/avatar-jessica.jpeg" alt="" />
+      <img src="../assets/images/avatar-jessica.jpeg" alt="Profile picture" />
       <h2 class="font-bold">Jessica Randall</h2>
       <p>London, United Kingdom</p>
 
