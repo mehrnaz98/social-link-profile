3742d51506de6e59436b0d0c5011d6e515211ccd
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 00:41:11 +0330

    Give the profile pic a width of 16 and a height of auto

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,7 +3,11 @@
     class="font-inter border w-[350px] h-[510px] flex flex-col justify-center items-center text-center"
   >
     <div class="flex flex-col">
-      <img src="../assets/images/avatar-jessica.jpeg" alt="Profile picture" />
+      <img
+        src="../assets/images/avatar-jessica.jpeg"
+        alt="Profile picture"
+        class="w-16 h-auto"
+      />
       <h2 class="font-bold">Jessica Randall</h2>
       <p>London, United Kingdom</p>
 
