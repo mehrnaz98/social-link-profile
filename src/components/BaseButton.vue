36be62e632000b7f347372e280cca059508846bd
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 13:34:04 +0330

    Move buttons into BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col space-y-3 pt-5 justify-center items-center">
+    <button
+      class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
+    >
+      GitHub
+    </button>
+    <button
+      class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
+    >
+      Frontend Mentor
+    </button>
+    <button
+      class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
+    >
+      LinkedIn
+    </button>
+    <button
+      class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
+    >
+      Twitter
+    </button>
+    <button
+      class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
+    >
+      Instagram
+    </button>
+  </div>
+</template>
