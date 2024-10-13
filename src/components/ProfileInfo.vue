b119129b2d2e81e21e1ab09eb976abb5df11f709
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 13:28:16 +0330

    Move the profile information into ProfileInfo component

---
+++ src/components/ProfileInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <img
+      src="../assets/images/avatar-jessica.jpeg"
+      alt="Profile picture"
+      class="w-[75px] h-auto border-none rounded-full"
+    />
+    <h2 class="font-semibold text-[#D3D3D3] text-xl pt-7">Jessica Randall</h2>
+    <p class="text-[#BBD37B] text-xs font-semibold pt-1">
+      London, United Kingdom
+    </p>
+
+    <p class="text-[#B7B7B7] text-xs pt-5">
+      "Front-end developer and avid reader."
+    </p>
+  </div>
+</template>
