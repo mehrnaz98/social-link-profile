0a945db01a85ecb6e4a5d41f1bba7722ae39fa30
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 13:31:06 +0330

    Import profile info component into base card

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -2,21 +2,7 @@
   <section
     class="bg-[#1F1F1F] font-inter border-none rounded-lg sm:w-[310px] h-[490px] w-[280px] flex flex-col justify-center items-center text-center"
   >
-    <div class="flex flex-col justify-center items-center">
-      <img
-        src="../assets/images/avatar-jessica.jpeg"
-        alt="Profile picture"
-        class="w-[75px] h-auto border-none rounded-full"
-      />
-      <h2 class="font-semibold text-[#D3D3D3] text-xl pt-7">Jessica Randall</h2>
-      <p class="text-[#BBD37B] text-xs font-semibold pt-1">
-        London, United Kingdom
-      </p>
-
-      <p class="text-[#B7B7B7] text-xs pt-5">
-        "Front-end developer and avid reader."
-      </p>
-    </div>
+    <ProfileInfo />
 
     <div class="flex flex-col space-y-3 pt-5 justify-center items-center">
       <button
@@ -47,3 +33,7 @@
     </div>
   </section>
 </template>
+
+<script setup>
+import ProfileInfo from "./ProfileInfo.vue";
+</script>
