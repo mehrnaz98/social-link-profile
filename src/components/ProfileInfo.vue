40ebdcb9e4cbdf2b0ce9569023335ea14e3d7e5d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 21:54:26 +0330

    Increase picture width

--- src/components/ProfileInfo.vue
+++ src/components/ProfileInfo.vue
@@ -4,7 +4,7 @@
       <img
         :src="user.avatar_url"
         alt="user profile photo"
-        class="w-[75px] h-auto border-none rounded-full m-auto -mb-3"
+        class="w-[88px] h-auto border-none rounded-full m-auto -mb-3"
       />
       <h2 class="font-semibold text-[#D9D9D9] text-xl pt-7">{{ user.name }}</h2>
       <p class="text-[#BBD37B] text-xs font-semibold pt-1">
@@ -20,14 +20,14 @@
         alt="user profile photo"
         class="w-[75px] h-auto border-none rounded-full m-auto -mb-3"
       />
-      <h2 class="font-semibold text-[#D9D9D9] text-xl pt-7">
+      <h2 class="font-semibold text-[#D9D9D9] text-2xl pt-7">
         Mehrnaz Eftekhari
       </h2>
-      <p class="text-[#BBD37B] text-xs font-semibold pt-1">
+      <p class="text-[#BBD37B] text-[14px] font-semibold pt-1">
         Rasht, Guilan, Iran
       </p>
 
-      <p class="text-[#B7B7B7] text-xs pt-5 px-4">
+      <p class="text-[#B7B7B7] text-[14px] pt-5 px-4">
         "Mechanical Engineer and Designer; Translator and Content Creator;
         Aspiring Frontend Developer."
       </p>
