54716d04629cbdc13ce352bd46145119d5cef78f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 15:31:38 +0330

    Change button to anchor text and give it a display of flex

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
-  <button
-    class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md hover:bg-[#C4F82A] hover:text-[#1F1F1F]"
+  <a
+    class="flex justify-center items-center bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md hover:bg-[#C4F82A] hover:text-[#1F1F1F]"
   >
     <slot></slot>
-  </button>
+  </a>
 </template>
