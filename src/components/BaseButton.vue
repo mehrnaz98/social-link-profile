96883971ce5e6eba37555e1ea648374a83069e72
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 22:05:36 +0330

    Increase width for mobile devices

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    class="flex justify-center items-center bg-[#333333] sm:w-[310px] w-[235px] py-3 text-[14px] font-semibold text-[#D9D9D9] border-none rounded-md hover:bg-[#C4F82A] transition-color duration-200 hover:text-[#1F1F1F] focus:outline-none focus:ring focus:ring-[#BBD37B]"
+    class="flex justify-center items-center bg-[#333333] sm:w-[310px] w-[290px] py-3 text-[14px] font-semibold text-[#D9D9D9] border-none rounded-md hover:bg-[#C4F82A] transition-color duration-200 hover:text-[#1F1F1F] focus:outline-none focus:ring focus:ring-[#BBD37B]"
     :href="url"
     target="_blank"
     rel="noopener noreferrer"
