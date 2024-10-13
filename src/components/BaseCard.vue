634f5fd73e0ba805b84f6cb41dbc36fcf05ee87d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 13:34:34 +0330

    Import BaseButton into BaseCard component

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,37 +3,11 @@
     class="bg-[#1F1F1F] font-inter border-none rounded-lg sm:w-[310px] h-[490px] w-[280px] flex flex-col justify-center items-center text-center"
   >
     <ProfileInfo />
-
-    <div class="flex flex-col space-y-3 pt-5 justify-center items-center">
-      <button
-        class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
-      >
-        GitHub
-      </button>
-      <button
-        class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
-      >
-        Frontend Mentor
-      </button>
-      <button
-        class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
-      >
-        LinkedIn
-      </button>
-      <button
-        class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
-      >
-        Twitter
-      </button>
-      <button
-        class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
-      >
-        Instagram
-      </button>
-    </div>
+    <BaseButton />
   </section>
 </template>
 
 <script setup>
+import BaseButton from "./BaseButton.vue";
 import ProfileInfo from "./ProfileInfo.vue";
 </script>
