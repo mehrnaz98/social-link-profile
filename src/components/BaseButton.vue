418c8d1f31814664e95709736a8fa232622ee0cd
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-13 15:09:00 +0330

    Change button bg and text color when hovered

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md"
+    class="bg-[#333333] sm:w-[250px] h-[35px] w-[235px] text-xs font-semibold text-[#D3D3D3] border-none rounded-md hover:bg-[#C4F82A] hover:text-[#1F1F1F]"
   >
     <slot></slot>
   </button>
