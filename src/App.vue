a06dde5a85a3c485b4ae35ade754a6418b702649
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-15 23:12:51 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <template>
-  <div
-    class="relative flex flex-col justify-center items-center h-screen bg-[#141414]"
-  >
-    <BaseCard />
-    <div
-      class="absolute bottom-3 text-[#D9D9D9] sm:text-xs text-[10px] text-center"
-    >
-      Challenge by
-      <a
-        href="https://www.frontendmentor.io?ref=challenge"
-        target="_blank"
-        class="text-[#BBD37B] hover:font-bold transition-fonts duration-200"
-        >Frontend Mentor</a
-      >. Coded by
-      <a
-        href="https://github.com/mehrnaz98/"
-        class="text-[#BBD37B] hover:font-bold transition-fonts duration-200"
-        >Mehrnaz Eftekhari</a
-      >.
-    </div>
-  </div>
+  <BaseCard />
+
+  <TheFooter />
 </template>
 
 <script setup>
 import BaseCard from "./components/BaseCard.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
