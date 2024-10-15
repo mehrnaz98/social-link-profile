8cac0a8cf5a558d5be4a9384f9e6a0fb710289fa
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-15 23:14:27 +0330

    Create TheFooter.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div
+      class="text-[#D9D9D9] sm:text-xs text-[10px] text-center bg-[#141414] py-2"
+    >
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        class="text-[#BBD37B] hover:font-bold transition-fonts duration-200"
+        >Frontend Mentor</a
+      >. Coded by
+      <a
+        href="https://github.com/mehrnaz98/"
+        class="text-[#BBD37B] hover:font-bold transition-fonts duration-200"
+        >Mehrnaz Eftekhari</a
+      >.
+    </div>
+  </footer>
+</template>
